4f75d27f852ab898cf8a53af4c2e0ef162ca8962
Joshua J <jdjamrosz@stu.naperville203.org>
2024-02-06 09:21:09 -0600

    struggling

--- assets/js/teacherParser.js
+++ assets/js/teacherParser.js
@@ -1,2 +1,15 @@
 const csv = require("csv-parser");
 const fs = require("fs");
+
+const readData = async function () {
+  const results = [];
+  await fs.createReadStream("teachers.csv")
+    .pipe(csv())
+    .on("data", (data) => results.push(data))
+    .on("end", () => {
+      return results;
+    });
+    await fs.pipe.
+    
+};
+console.log(readData());
