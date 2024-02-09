7f4f0e37483aa2305258945700082ef903ac294d
JIMathew <111530516+JIMathew@users.noreply.github.com>
2024-02-09 09:27:33 -0600

    Courses json 💀💀💀💀 (#10)
    * edited json files
    
    * Teachers addition 👌👌👌👌 (#7)
    
    * begin full implementation of the teachers, step into full realization of project
    
    * Rewrote the teachers json;
    used wrong spreadsheet before;
    about 10 teachers added
    
    * finished, but left bci and yb teacher role slots unincluded
    
    * only semester 2 courses added, and special ones like ECE that have unique restrictions are treated about normally, for now
    
    * lol had to update classroom periods
    
    * more fixes :))))
    
    * connor did sumthing
    
    * replace Ditch
    
    ---------
    
    Co-authored-by: Ayarfox <abmistry2@stu.naperville203.org>

--- Sprint1/Scheduler.js
+++ Sprint1/Scheduler.js
@@ -93,7 +93,7 @@ let printInCoolWay = function (arr) {
 
 //update sections by priority
 let updateSections = function (arr) {
-  arr.sort((a, b) => a.course.schedulingPriority - b.course.schedulingPriority);
+  arr.sort((a, b) => b.course.schedulingPriority - a.course.schedulingPriority);
   return arr;
 };
 
@@ -113,7 +113,8 @@ let createSections = function (arr) {
       });
     }
   }
-  updateSections(arr);
+  arr = updateSections(arr);
+  console.log(arr);
   return arr;
 };
 
