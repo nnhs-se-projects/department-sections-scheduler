b0f809a9b36b27f495a902caec0e234caca16238
Joshua J <jdjamrosz@stu.naperville203.org>
2024-02-29 09:19:20 -0600

    JS is not a real language

--- assets/js/coursesEdit.js
+++ assets/js/coursesEdit.js
@@ -1,7 +1,23 @@
 const courseSelector = document.getElementById("coursePicker");
+const courseNameSelector = document.getElementById("namer");
+const courseSectionSelector = document.getElementById("sectionSelector");
+
+let currentCourse;
+let courseArr = document.getElementById("courseArr");
+
+const onStart = function () {
+  currentCourse = courseSelector.value;
+  if (currentCourse === "addCourse") {
+    return;
+  }
+  courseNameSelector.value = currentCourse;
+};
 
 courseSelector.addEventListener("change", () => {
+  console.log("funny");
   if (courseSelector.value === "addCourse") {
   } else {
   }
 });
+
+onStart();
