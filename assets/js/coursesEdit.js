689d9aa6774f4236d90e98e12da106a90007a0f8
Ayarfox <abmistry2@stu.naperville203.org>
2024-03-05 15:01:02 -0600

    priority edits

--- assets/js/coursesEdit.js
+++ assets/js/coursesEdit.js
@@ -1,6 +1,9 @@
 const courseSelector = document.getElementById("coursePicker");
 const courseNameSelector = document.getElementById("namer");
 const courseSectionSelector = document.getElementById("sectionSelector");
+const coursePrioritySelector = document.getElementById("prioritySelector");
+const coursePriorityToggle = document.getElementById("priorityOverrideEnabler");
+
 const coursePeriodsSelectors = [];
 
 for (let i = 1; i <= 8; i++) {
@@ -32,8 +35,7 @@ const onStart = async function () {
       }
 
       courseNameSelector.value = currentCourseName;
-      updateCoursePeriodsSelector();
-      updateCourseSectionSelector();
+      updateFields();
     })
   );
 };
@@ -49,22 +51,52 @@ const updateCourseSectionSelector = function () {
   courseSectionSelector.value = currentCourse.sections;
 };
 
-courseSelector.addEventListener("change", () => {
+const updateCoursePrioritySelector = function () {
+  if (currentCourse.userPriority === undefined) {
+    coursePriorityToggle.checked = false;
+    document
+      .getElementById("sectionSelectorDiv")
+      .setAttribute("class", "hidden");
+  } else {
+    coursePriorityToggle.checked = true;
+    document.getElementById("sectionSelectorDiv").setAttribute("class", "");
+    coursePrioritySelector.value = currentCourse.userPriority;
+  }
+};
+
+coursePriorityToggle.addEventListener("change", () => {
+  if (coursePriorityToggle.checked) {
+    document.getElementById("sectionSelectorDiv").setAttribute("class", "");
+  } else {
+    document
+      .getElementById("sectionSelectorDiv")
+      .setAttribute("class", "hidden");
+  }
+});
+
+courseSelector.addEventListener("change", () => updateFields());
+
+const updateFields = function () {
   currentCourseName = courseSelector.value;
-  currentCourse = courseArr.filter(
-    (course) => course.name === currentCourseName
-  )[0];
   if (courseSelector.value === "addCourse") {
+    currentCourse = null;
     courseNameSelector.value = "New Course";
     for (let i = 0; i < 8; i++) {
       coursePeriodsSelectors[i].checked = true;
     }
     courseSectionSelector.value = 0;
   } else {
+    currentCourse = courseArr.filter(
+      (course) => course.name === currentCourseName
+    )[0];
     courseNameSelector.value = currentCourseName;
     updateCoursePeriodsSelector();
     updateCourseSectionSelector();
+    updateCoursePrioritySelector();
   }
-});
+};
 
+//FIXME: we need to add a check to see if the course name is already in the database
+//FIXME: add a save button
+//FIXME: require all classrooms
 onStart();
