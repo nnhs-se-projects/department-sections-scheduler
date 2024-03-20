a135b42e446641cbf06938a8159b45bb289f9934
Ayarfox <abmistry2@stu.naperville203.org>
2024-03-20 15:09:07 -0500

    Removed 2 more FIXMEs

--- assets/js/classroomsEdit.js
+++ assets/js/classroomsEdit.js
@@ -274,6 +274,4 @@ const SaveData = {
 
 onStart();
 
-//FIXME: Need to ensure that courses with dependencies of rooms that are RENAMED have those rooms RENAMED
-//FIXME: Re-read the code and ensure no errors
 //FIXME: Sort the rooms by room number/name
