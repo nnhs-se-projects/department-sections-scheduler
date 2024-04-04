a1d969e0598f80211a37631dc19e8da1805cfebc
Ayarfox <abmistry2@stu.naperville203.org>
2024-04-04 09:38:01 -0500

    Transitions :thumbsup:

--- assets/js/dataView.js
+++ assets/js/dataView.js
@@ -1,14 +1,16 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
+const coll = document.getElementsByClassName("collapsible");
+let i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
+    const content = this.nextElementSibling;
     if (content.style.maxHeight) {
       content.style.maxHeight = null;
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
+      content.style.borderRadius = "0px 0px 10px 10px";
+      coll[i].style.borderRadius = "10px 10px 0px 0px";
     }
   });
 }
